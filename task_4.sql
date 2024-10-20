f769c71be7e9f124698e4d7bd7649ee594a3952b
josiah37 <eyosi50@gmail.com>
2024-10-20 23:59:16 +0300

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,5 @@
--- Use the specified database  
-USE alx_book_store;  
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+AND TABLE_SCHEMA = DATABASE();
 
--- Query to get the description of the books table  
-SELECT   
-    COLUMN_NAME,   
-    DATA_TYPE,   
-    IS_NULLABLE,   
-    COLUMN_DEFAULT,   
-    CHARACTER_MAXIMUM_LENGTH   
-FROM   
-    INFORMATION_SCHEMA.COLUMNS   
-WHERE   
-    TABLE_SCHEMA = 'alx_book_store'   
-    AND TABLE_NAME = 'Books';
